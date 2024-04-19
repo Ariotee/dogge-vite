6d62b7e7f02b795f50cf0f8417b8762649a82fcd
SoloTufi <mat_990011@mail.ru>
2024-04-19 20:17:52 +0700

    alias
    Signed-off-by: SoloTufi <mat_990011@mail.ru>

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ export default defineConfig({
 			'@': path.resolve(__dirname, './src'),
 			'@assets': path.resolve(__dirname, './src/assets'),
 			'@common': path.resolve(__dirname, './src/common'),
+			'@pages': path.resolve(__dirname, './src/pages'),
 		},
 	},
 	plugins: [react()],
