383c9c3e734ad84ccff72f6632de54c3bad02c74
SoloTufi <mat_990011@mail.ru>
2024-04-19 20:19:05 +0700

    inputs
    Signed-off-by: SoloTufi <mat_990011@mail.ru>

--- src/common/fields/inputs/input/Input.tsx
+++ src/common/fields/inputs/input/Input.tsx
@@ -1,20 +1,38 @@
 import React from 'react';
 
-import style from './input.module.css';
+import style from '../input.module.css';
 
-interface InputProps extends React.HTMLProps<HTMLInputElement> {
+interface InputProps extends Omit<React.InputHTMLAttributes<HTMLInputElement>, 'placeholder'> {
+	label: string;
 	isError?: boolean;
 	helperText?: string;
 }
 
-export const Input: React.FC<InputProps> = ({ isError = false, helperText, ...props }) => {
+export const Input: React.FC<InputProps> = ({ isError = false, helperText, label, ...props }) => {
+	const inputRef = React.useRef<HTMLInputElement>(null);
+	const [isFocus, setIsFocus] = React.useState(!!props.value ?? false);
+
 	return (
 		<>
-			<input
-				className={`${style.input} ${isError ? style.error : ''} `}
-				type='text'
-				{...props}
-			/>
+			<div
+				className={`${style.input_container} ${isError ? style.input_container : ''} ${
+					isFocus ? style.focused : ''
+				}`}
+				onClick={() => {
+					inputRef.current?.focus();
+					setIsFocus(true);
+				}}
+			>
+				<label htmlFor='' className={style.input_label}>
+					{label}
+				</label>
+				<input
+					onBlur={() => !props.value && setIsFocus(false)}
+					ref={inputRef}
+					className={style.input}
+					{...props}
+				/>
+			</div>
 			{isError && helperText && <span className={style.helper_text}>{helperText}</span>}
 		</>
 	);
