9491e2c7956cb4905a02c2ca5dab0238661a2220
SoloTufi <mat_990011@mail.ru>
2024-04-19 19:44:54 +0700

    Routes
    Signed-off-by: SoloTufi <mat_990011@mail.ru>

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,29 @@
-import './App.scss';
+import React from 'react';
+
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+
+import { LoginPage, NotFoundPage, RegistrationPage } from '@pages/index';
+import './scss/App.scss';
+
+const AuthRoutes = () => (
+	<Routes>
+		<Route path='/auth' element={<LoginPage />} />
+		<Route path='/registration' element={<RegistrationPage />} />
+		<Route path='*' element={<Navigate to='/auth' />} />
+	</Routes>
+);
+
+const MainRoutes = () => (
+	<Routes>
+		<Route path='*' element={<NotFoundPage />} />
+	</Routes>
+);
 
 function App() {
-	return (
-		<div className=''>
-			<div className=''></div>
-		</div>
-	);
+	//@ts-ignore
+	const [isAuth, setIsAuth] = React.useState(false);
+
+	return <BrowserRouter>{isAuth ? <MainRoutes /> : <AuthRoutes />}</BrowserRouter>;
 }
 
 export default App;
